89f27cee92fa9f2446f47390c9b5a4db52678024
masonz <masonzone@gmali.com>
2017-09-14 20:32:26 +0800

    test: add unit test

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,15 +3,21 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-vue-ts-starter:app', () => {
-  beforeAll(() => {
+describe('generator-vue-ts-starter', () => {
+  beforeEach(() => {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+      .withPrompts({ vuex: true });
   });
 
-  it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+  it('creates some files', () => {
+    const expected = [
+      '.gitgnore',
+      'README.md',
+      'package.json',
+      'karma.coverage.js',
+      'webpack.config.js'
+    ];
+
+    assert.file(expected);
   });
 });
